c466c0333e04b43c495bd9b587253931dd4bc8b7
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2024-02-04 15:00:12 -0500

    Update eslint and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,21 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:node/recommended',
+        'plugin:jsdoc/recommended',
+        'plugin:tailwindcss/recommended',
+        'plugin:@peggyjs/recommended'
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs', 'grammar.pegjs'],
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
     settings: { react: { version: '18.2' } },
-    plugins: ['react-refresh', 'prettier'],
+    plugins: ['react-refresh', 'prettier', 'import', 'node', 'jsdoc', 'tailwindcss', 'comment-length', 'sort-exports'],
     rules: {
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'prettier/prettier': 'off',
     },
 };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,16 @@
+import './App.css';
+
 import { useState } from 'react';
 
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
+/**
+ *
+ */
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
+            <div></div>
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
