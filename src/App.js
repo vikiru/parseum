dd42ed8d3a5f5854fdd3c362734cac53cde61fb7
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-01-05 19:33:21 -0500

    chore: format files with biome

--- src/App.js
+++ src/App.js
@@ -5,90 +5,89 @@ import NavBar from './components/NavBar/NavBar.jsx';
 import { parse } from './parser/parser.js';
 
 function useDebounce(value, delay) {
-    const [debouncedValue, setDebouncedValue] = useState(value);
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedValue(value);
-        }, delay);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
 
-        return () => {
-            clearTimeout(handler);
-        };
-    }, [value, delay]);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
 
-    return debouncedValue;
+  return debouncedValue;
 }
 
 function App() {
-    const [text, setText] = useState('');
-    const [html, setHTML] = useState('');
-    const debouncedText = useDebounce(text, 500);
-    const textareaRef = useRef(null);
-    const divRef = useRef(null);
+  const [text, setText] = useState('');
+  const [html, setHTML] = useState('');
+  const debouncedText = useDebounce(text, 500);
+  const textareaRef = useRef(null);
+  const divRef = useRef(null);
 
-    useEffect(() => {
-        const savedMarkdown = localStorage.getItem('markdown');
-        if (savedMarkdown) {
-            setText(savedMarkdown);
-        }
-    }, []);
+  useEffect(() => {
+    const savedMarkdown = localStorage.getItem('markdown');
+    if (savedMarkdown) {
+      setText(savedMarkdown);
+    }
+  }, []);
 
-    useEffect(() => {
-        if (debouncedText) {
-            const parsedHTML = parse(debouncedText);
-            setHTML(parsedHTML.html);
-        } else {
-            setHTML('');
-        }
-    }, [debouncedText]);
+  useEffect(() => {
+    if (debouncedText) {
+      const parsedHTML = parse(debouncedText);
+      setHTML(parsedHTML.html);
+    } else {
+      setHTML('');
+    }
+  }, [debouncedText]);
 
-    const handleChange = (event) => {
-        setText(event.target.value);
-    };
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
 
-    const handleScroll = () => {
-        if (textareaRef.current && divRef.current) {
-            const textareaScrollRatio =
-                textareaRef.current.scrollTop / (textareaRef.current.scrollHeight - textareaRef.current.clientHeight);
-            divRef.current.scrollTop =
-                textareaScrollRatio * (divRef.current.scrollHeight - divRef.current.clientHeight);
-        }
-    };
+  const handleScroll = () => {
+    if (textareaRef.current && divRef.current) {
+      const textareaScrollRatio =
+        textareaRef.current.scrollTop / (textareaRef.current.scrollHeight - textareaRef.current.clientHeight);
+      divRef.current.scrollTop = textareaScrollRatio * (divRef.current.scrollHeight - divRef.current.clientHeight);
+    }
+  };
 
-    const saveMarkdown = () => {
-        localStorage.setItem('markdown', text);
-    };
+  const saveMarkdown = () => {
+    localStorage.setItem('markdown', text);
+  };
 
-    const clearMarkdown = () => {
-        setText('');
-        localStorage.removeItem('markdown');
-    };
+  const clearMarkdown = () => {
+    setText('');
+    localStorage.removeItem('markdown');
+  };
 
-    return (
-        <div>
-            <NavBar saveMarkdown={saveMarkdown} clearMarkdown={clearMarkdown} />
-            <div className="flex">
-                <section id="markdown-input" className="m-2 w-1/2">
-                    <textarea
-                        id="input"
-                        ref={textareaRef}
-                        placeholder="Type your markdown here. View the rendered HTML on the right."
-                        className="textarea textarea-bordered textarea-lg h-full w-full"
-                        rows={20}
-                        value={text}
-                        onChange={handleChange}
-                        onScroll={handleScroll}
-                        style={{ resize: 'none' }}
-                    ></textarea>
-                </section>
-                <section id="html-output" className="m-2 w-1/2 overflow-auto">
-                    <div ref={divRef} className="prose pl-6" dangerouslySetInnerHTML={{ __html: html }} />
-                </section>
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <NavBar clearMarkdown={clearMarkdown} saveMarkdown={saveMarkdown} />
+      <div className="flex">
+        <section className="m-2 w-1/2" id="markdown-input">
+          <textarea
+            className="textarea textarea-bordered textarea-lg h-full w-full"
+            id="input"
+            onChange={handleChange}
+            onScroll={handleScroll}
+            placeholder="Type your markdown here. View the rendered HTML on the right."
+            ref={textareaRef}
+            rows={20}
+            style={{ resize: 'none' }}
+            value={text}
+          ></textarea>
+        </section>
+        <section className="m-2 w-1/2 overflow-auto" id="html-output">
+          <div className="prose pl-6" dangerouslySetInnerHTML={{ __html: html }} ref={divRef} />
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
