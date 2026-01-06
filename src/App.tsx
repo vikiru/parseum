066eab81528b3243edc426753d420cb379c5e79e
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-01-05 19:53:10 -0500

    refactor: move to vite from cra

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Footer from './components/Footer/Footer.jsx';
 import NavBar from './components/NavBar/NavBar.jsx';
